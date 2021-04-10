ac0614d671d388e21cf1f6f9def0480d2b6d10ef
Konrad Trzciński <trzcinskiphoto@gmail.com>
2021-04-10 22:31:27 +0200

    Fixed token verification
    Incorrect usage of .length on number,
    fixed with .toString()

--- sem2/tetris-server/classes/RequestAuthValidator.js
+++ sem2/tetris-server/classes/RequestAuthValidator.js
@@ -1,15 +1,19 @@
 class RequestAuthValidator {
-    constructor(request) {
-        this.token = request.headers['auth-token'] || request.body['auth-token'];
-    }
+  constructor(request) {
+    this.token = request.headers["auth-token"] || request.body["auth-token"];
+  }
 
-    validate() {
-        return this.token && this.token.length === 4 && isNaN(this.token) === false;
-    }
+  validate() {
+    return (
+      this.token &&
+      this.token.toString().length === 4 &&
+      isNaN(this.token) === false
+    );
+  }
 
-    getToken() {
-        return this.token;
-    }
+  getToken() {
+    return this.token;
+  }
 }
 
-module.exports = RequestAuthValidator
+module.exports = RequestAuthValidator;
